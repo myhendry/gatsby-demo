a0de7dba6f5c1110a3d4d9b0f4be4f0e2ea15d46
Hendry Lim <myhendry@gmail.com>
2020-03-01 15:15:19 +0800

    added file

---
+++ src/services/Firebase/config.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDdLgtYll95TUIL_e8vsk-UYitNE9OfH9E",
+  authDomain: "industrial-ee722.firebaseapp.com",
+  databaseURL: "https://industrial-ee722.firebaseio.com",
+  projectId: "industrial-ee722",
+  storageBucket: "industrial-ee722.appspot.com",
+  messagingSenderId: "528806960875",
+  appId: "1:528806960875:web:db21f3f1d78d9624fd28ae",
+  measurementId: "G-LBC0LPJ3DH",
+}
+
+export default firebaseConfig
